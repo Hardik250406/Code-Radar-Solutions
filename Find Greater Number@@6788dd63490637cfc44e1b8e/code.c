3e9289e028f534d39b6379621b792545eab138bc
Hardik250406 <hardik0406.becse24@chitkara.edu.in>
2025-02-12 23:41:10 +0530

    Solution to Find Greater Number

--- Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
+++ Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
@@ -1,34 +1,37 @@
 // // Your code here...
-// #include<stdio.h>
-// int main(){
-//     int a,b;
-//     scanf("%d%d",&a,&b);
-//     if( a > b ){
-//         printf("%d",a);
-    
-//     }
-//     else ( a < b ){
-//         printf( "%d"  , b );
-//     }
-   
-//     return 0;
-// }
-
-#include <stdio.h>
-
-int main() {
+#include<stdio.h>
+int main(){
     int a, b;
-    scanf("%d%d", &a, &b);  // Correct format
-
-    if (a > b) {
-        printf("%d", a);  // Corrected printf
-    } 
-    else if (a < b) {  // Fixed else if condition
-        printf("%d", b);  // Corrected printf
-    } 
-    else {  // Added else case to handle equal numbers
-        printf("Both numbers are equal");
+    scanf("%d%d", &a, &b);
+    if ( a > b ){
+        printf("%d",a);
+    
     }
-
+    else if ( a < b ){
+        printf( "%d"  , b );
+    }
+    else{
+        printf("0")
+    }
+   
     return 0;
 }
+
+// #include <stdio.h>
+
+// int main() {
+//     int a, b;
+//     scanf("%d%d", &a, &b);  // Correct format
+
+//     if (a > b) {
+//         printf("%d", a);  // Corrected printf
+//     } 
+//     else if (a < b) {  // Fixed else if condition
+//         printf("%d", b);  // Corrected printf
+//     } 
+//     else {  // Added else case to handle equal numbers
+//         printf("0");
+//     }
+
+//     return 0;
+// }
