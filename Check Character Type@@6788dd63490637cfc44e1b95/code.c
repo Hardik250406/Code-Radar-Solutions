2d1dde220f95231163b5b22341eb39630636bf50
Hardik250406 <hardik0406.becse24@chitkara.edu.in>
2025-02-13 09:44:19 +0530

    Solution to Check Character Type

---
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -0,0 +1,12 @@
+// Your code here...
+#include<stdio.h>
+int main(){
+    char X;
+    int A;
+    spl S;
+    scanf("%c %d %s",&X,&A,&S);
+    if ((X<a) && (X>z) || (X<A) && (X>Z)){
+        printf("Vowel");
+    }
+    return 0;
+}
