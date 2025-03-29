b7b6e5d607ddd33f08c7dff4c3873376d995fd71
Hardik250406 <hardik0406.becse24@chitkara.edu.in>
2025-03-29 14:16:28 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -5,10 +5,10 @@ if(n==0)return 0;
 if(n==1)return 1;
 return fibonacciSeries(n-1)+fibonacciSeries(n-2);
 }
-printf("%d",fibonacciSeries(i));
 
 
-// for (int i=0;i<=n;i++){
-    
 
-// }
+for (int i=0;i<=n;i++){
+    printf("%d",fibonacciSeries(i));
+
+}
